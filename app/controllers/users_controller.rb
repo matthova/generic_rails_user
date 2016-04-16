f91e27de6fa0b10b6b9f0e0e5f575d9555c11d86
hovanem <matt.hovanec@autodesk.com>
2016-04-15 18:01:35 -0700

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
